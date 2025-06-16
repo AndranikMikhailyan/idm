b996ae00289cea6619b6cfc164eae2e889d0eb1f
Andranik <andranik.st@gmail.com>
2025-06-16 21:38:00 +0300

    add migrations

--- migration/create-role.sql
+++
@@ -1,7 +0,0 @@
-create table role
-(
-    id         bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name       varchar     NOT NULL UNIQUE,
-    created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
